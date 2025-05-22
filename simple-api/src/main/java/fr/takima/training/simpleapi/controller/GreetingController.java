1f2a33452d858fee93ff3ad0126ec0fbced5e2ed
FrancisLabrele <francois.thievon@epfedu.fr>
2025-05-22 08:29:47 +0200

    Nouvelle page

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -1,8 +1,11 @@
 package fr.takima.training.simpleapi.controller;
 
-import org.springframework.web.bind.annotation.*;
-
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
@@ -11,7 +14,7 @@ public class GreetingController {
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "François !") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
